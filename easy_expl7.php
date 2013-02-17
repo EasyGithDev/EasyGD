8102993ced6caaf68a64be7b6f4771b55947af8b
EasyGithDev <florent.brusciano@gmail.com>
2013-02-17 11:39:49 +0100

    using the observer pattern for running through the image

--- easy_expl7.php
+++ easy_expl7.php
@@ -1,23 +1,33 @@
 <?php
 
+set_time_limit(0);
+
 require 'autoload.php';
 
 $filename = 'http://static.zend.com/topics/ZF2-for-ZF-site-logo-01-B-350.png';
 
-/*
- * 
- * How to have the histogramme
- * 
- */
-if(($image = Easy\Image::createfrom($filename)) === FALSE)
+if (($image = Easy\Image::createfrom($filename)) === FALSE)
     throw new Exception('Unable to load ' . $filename);
 
-$histo = Easy\Histogramme::create($image)->process();
+$progress = new Easy\Progress();
+$thresholding = new Easy\Thresholding(128);
+$negative = new Easy\Negative();
+$histogramme = new Easy\Histogramme();
 
-echo '<pre>',  print_r($histo->getHisto(), 1),'</pre>';
-echo '<pre>',  print_r($histo->getAverage(), 1),'</pre>';
-echo '<pre>',  print_r($histo->getSigma(), 1),'</pre>';
+$runer = Easy\RunThrough::create($image);
+$runer->attach($progress);
+$runer->attach($histogramme);
+$runer->attach($negative);
+$runer->process();
 
-$histo->save();
+$runer->getImageDest()->show();
+
+$histogramme->computeSigma();
+
+//echo '<pre>',  print_r($histo->getHisto(), 1),'</pre>';
+//echo '<pre>',  print_r($histo->getAverage(), 1),'</pre>';
+//echo '<pre>',  print_r($histo->getSigma(), 1),'</pre>';
+
+$histogramme->save();
 
 ?>
