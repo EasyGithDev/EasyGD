531c67a3fcd8809ac6a58a5d84739c1a2cc536b2
EasyGithDev <florent.brusciano@gmail.com>
2013-02-17 12:53:50 +0100

    example of the src method

--- easy_expl4.php
+++ easy_expl4.php
@@ -1,5 +1,4 @@
 <?php
-
 require 'autoload.php';
 
 $filename = 'http://static.zend.com/topics/ZF2-for-ZF-site-logo-01-B-350.png';
@@ -7,8 +6,28 @@ $filename = 'http://static.zend.com/topics/ZF2-for-ZF-site-logo-01-B-350.png';
 /*
  * 
  * How to load an image for data src
+ * Use it only on small image, if you dont want that your html page becommes to big 
+ */
+?>
+
+<img src="<?php echo Easy\Image::getDataSource($filename); ?>" />
+
+<?php
+/*
+ * It's VERY DANGEROUS to use this function in production. 
+ * 
+ * The src function use a temporary file to send the data.
+ * Use only this function for testing your application.
  * 
  */
 
+$image = Easy\Image::createfrom($filename)
+        ->addText(
+        Easy\Text::create('Hello Zend')
+        ->setColor(Easy\Color::Silver())
+        ->setSize(4)
+        ->setPosition(Easy\Position::create(60, 20))
+        );
 ?>
-<img src="<?php echo Easy\Image::getDataSource($filename); ?>" />
+
+<img src="<?php echo $image->src(); ?>">
