a30776f470afc49172ef647e8eff84bb4b46b663
EasyGithDev <florent.brusciano@gmail.com>
2013-02-27 23:26:36 +0100

    fix bug

--- Easy/histogramme.php
+++ Easy/histogramme.php
@@ -41,140 +41,141 @@ namespace Easy;
 class Histogramme implements \SplObserver {
 
     private $histo = array(
-        'red' => array(),
-        'green' => array(),
-        'blue' => array(),
-        'alpha' => array()
+	'red' => array(),
+	'green' => array(),
+	'blue' => array(),
+	'alpha' => array()
     );
     private $max = array(
-        'red' => 0,
-        'green' => 0,
-        'blue' => 0,
-        'alpha' => 0
+	'red' => 0,
+	'green' => 0,
+	'blue' => 0,
+	'alpha' => 0
     );
     private $average = array(
-        'red' => 0,
-        'green' => 0,
-        'blue' => 0,
-        'alpha' => 0
+	'red' => 0,
+	'green' => 0,
+	'blue' => 0,
+	'alpha' => 0
     );
     private $sigma = array(
-        'red' => 0,
-        'green' => 0,
-        'blue' => 0,
-        'alpha' => 0
+	'red' => 0,
+	'green' => 0,
+	'blue' => 0,
+	'alpha' => 0
     );
     private $max_height = 256;
 
     public function __construct() {
-        foreach ($this->histo as $k => &$v) {
-            $v = array_fill(0, 256, 0);
-        }
+	foreach ($this->histo as $k => &$v) {
+	    $v = array_fill(0, 256, 0);
+	}
     }
 
-    public function computeSigma() {
-        $var = array(
-            'red' => 0,
-            'green' => 0,
-            'blue' => 0,
-            'alpha' => 0
-        );
+    public function computeSigma($pixels) {
+	$var = array(
+	    'red' => 0,
+	    'green' => 0,
+	    'blue' => 0,
+	    'alpha' => 0
+	);
 
-        $div = floatval(1 / $pixels);
-        foreach ($this->histo as $k => $v) {
-            $sum = 0;
-            $sum2 = 0;
-            for ($i = 0; $i < 255; $i++) {
-                $sum += $v[$i] * $i;
-                $sum2 += $v[$i] * $i * $i;
-            }
-            $this->average[$k] = round($sum * $div);
-            $var[$k] = $sum2 * $div - $sum * $div * $sum * $div;
-            $this->sigma[$k] = round(sqrt($var[$k]));
-        }
+	$div = floatval(1 / $pixels);
+	foreach ($this->histo as $k => $v) {
+	    $sum = 0;
+	    $sum2 = 0;
+	    for ($i = 0; $i < 255; $i++) {
+		$sum += $v[$i] * $i;
+		$sum2 += $v[$i] * $i * $i;
+	    }
+	    $this->average[$k] = round($sum * $div);
+	    $var[$k] = $sum2 * $div - $sum * $div * $sum * $div;
+	    $this->sigma[$k] = round(sqrt($var[$k]));
+	}
     }
 
-    public function save() {
-        $dimension = Dimension::create($this->max_height, $this->max_height);
-        $white = Color::White();
-        $red = Color::Red();
-        $green = Color::Green();
-        $blue = Color::Blue();
-        foreach ($this->histo as $k => $v) {
-            $ratio = $this->max_height / $this->max[$k];
-            $image = Image::create($dimension)->fill($white);
+    public function save($path) {
 
-            $p1 = Position::create(0, $this->max_height);
-            $p2 = Position::create(0, $this->max_height);
+	$dimension = Dimension::create($this->max_height, $this->max_height);
+	$white = Color::White();
+	$red = Color::Red();
+	$green = Color::Green();
+	$blue = Color::Blue();
+	foreach ($this->histo as $k => $v) {
+	    $ratio = $this->max_height / $this->max[$k];
+	    $image = Image::create($dimension)->fill($white);
 
-            for ($i = 0; $i < 255; $i++) {
+	    $p1 = Position::create(0, $this->max_height);
+	    $p2 = Position::create(0, $this->max_height);
 
-                $p1->setX($i);
-                $p2->setX($i);
-                $p2->setY($this->max_height - round($v[$i] * $ratio));
+	    for ($i = 0; $i < 255; $i++) {
 
-                $image->drawLine($p1, $p2, $$k);
-            }
-            $image->save($k);
-        }
+		$p1->setX($i);
+		$p2->setX($i);
+		$p2->setY($this->max_height - round($v[$i] * $ratio));
+
+		$image->drawLine($p1, $p2, $$k);
+	    }
+	    $image->save($path .'/'. $k);
+	}
     }
 
     public function update(\SplSubject $obj) {
 
-        $x = $obj->getColumn();
-        $y = $obj->getLine();
-        $imageSrc = $obj->getImageSrc();
-        $pixels = $imageSrc->getWidth() * $imageSrc->getHeight();
+	$x = $obj->getColumn();
+	$y = $obj->getLine();
+	$imageSrc = $obj->getImageSrc();
 
-        $rgb = imagecolorat($imageSrc->getImg(), $x, $y);
-        $r = ($rgb >> 16) & 0xFF;
-        $g = ($rgb >> 8) & 0xFF;
-        $b = $rgb & 0xFF;
 
-        if ($r > 255)
-            $r = 255;
-        if ($g > 255)
-            $g = 255;
-        if ($b > 255)
-            $b = 255;
+	$rgb = imagecolorat($imageSrc->getImg(), $x, $y);
+	$r = ($rgb >> 16) & 0xFF;
+	$g = ($rgb >> 8) & 0xFF;
+	$b = $rgb & 0xFF;
 
-        if ($r < 0)
-            $r = 0;
-        if ($g < 0)
-            $g = 0;
-        if ($b < 0)
-            $b = 0;
+	if ($r > 255)
+	    $r = 255;
+	if ($g > 255)
+	    $g = 255;
+	if ($b > 255)
+	    $b = 255;
 
-        if ($r != 0)
-            $this->histo['red'][$r]++;
-        if ($g != 0)
-            $this->histo['green'][$g]++;
-        if ($b != 0)
-            $this->histo['blue'][$b]++;
+	if ($r < 0)
+	    $r = 0;
+	if ($g < 0)
+	    $g = 0;
+	if ($b < 0)
+	    $b = 0;
 
-        if ($this->histo['red'][$r] > $this->max['red'])
-            $this->max['red'] = $this->histo['red'][$r];
+	if ($r != 0)
+	    $this->histo['red'][$r]++;
+	if ($g != 0)
+	    $this->histo['green'][$g]++;
+	if ($b != 0)
+	    $this->histo['blue'][$b]++;
 
-        if ($this->histo['green'][$g] > $this->max['green'])
-            $this->max['green'] = $this->histo['green'][$g];
+	if ($this->histo['red'][$r] > $this->max['red'])
+	    $this->max['red'] = $this->histo['red'][$r];
 
-        if ($this->histo['blue'][$b] > $this->max['blue'])
-            $this->max['blue'] = $this->histo['blue'][$b];
+	if ($this->histo['green'][$g] > $this->max['green'])
+	    $this->max['green'] = $this->histo['green'][$g];
 
-        unset($this->histo['alpha']);
-        unset($this->max['alpha']);
+	if ($this->histo['blue'][$b] > $this->max['blue'])
+	    $this->max['blue'] = $this->histo['blue'][$b];
+
+	unset($this->histo['alpha']);
+	unset($this->max['alpha']);
     }
 
     public function getHisto() {
-        return $this->histo;
+	return $this->histo;
     }
 
     public function getAverage() {
-        return $this->average;
+	return $this->average;
     }
 
     public function getSigma() {
-        return $this->sigma;
+	return $this->sigma;
     }
 
 }
