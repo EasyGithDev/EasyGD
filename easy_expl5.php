2d08bbd0e322b8134647c06a9d8eb16ea05ddbfb
EasyGithDev <florent.brusciano@gmail.com>
2013-03-04 01:00:51 +0100

    change the inlay method

--- easy_expl5.php
+++ easy_expl5.php
@@ -2,8 +2,8 @@
 
 require 'autoload.php';
 
-//$filename = 'http://static.zend.com/topics/ZF2-for-ZF-site-logo-01-B-350.png';
-$filename = 'http://www.php.net/images/logos/php-med-trans.png';
+$filename = 'http://static.zend.com/topics/ZF2-for-ZF-site-logo-01-B-350.png';
+//$filename = 'http://www.php.net/images/logos/php-med-trans.png';
 //$filename = 'http://intechgriti.in/wp-content/uploads/2012/06/php-frameworks.gif';
 
 /*
@@ -51,13 +51,13 @@ if (($image = Easy\Image::createFrom($filename)) === FALSE)
  */
 
 /*
-$filename = 'http://static.php.net/www.php.net/images/php.gif';
-Easy\Transformation::crop(
-        Easy\Image::createFrom($filename), 
-        Easy\Position::create(20, 13), 
-        Easy\Dimension::create(80, 40))
-        ->show();
-*/
+  $filename = 'http://static.php.net/www.php.net/images/php.gif';
+  Easy\Transformation::crop(
+  Easy\Image::createFrom($filename),
+  Easy\Position::create(20, 13),
+  Easy\Dimension::create(80, 40))
+  ->show();
+ */
 
 
 /*
@@ -73,7 +73,7 @@ Easy\Transformation::crop(
  * How to make a rotation
  * 
  */
-Easy\Transformation::rotate($image, 90)->show();
+//Easy\Transformation::rotate($image, 90)->show();
 
 
 /*
@@ -81,24 +81,24 @@ Easy\Transformation::rotate($image, 90)->show();
  * How to add merge two images
  * 
  */
-//$logo = Easy\Image::createFrom('http://www.php.net/images/logos/php-med-trans.png');
-//$logo = Easy\Transformation::resizeByPercent($logo, 0.6);
-//Easy\Transformation::inlay($image, $logo, Easy\Transformation::POSITION_MIDDLE_RIGHT, Easy\Position::create(30, 10), 50 )->show();
+$logo = Easy\Image::createFrom('http://www.php.net/images/logos/php-med-trans.png');
+$logo = Easy\Transformation::resizeByPercent($logo, 0.6);
+Easy\Transformation::inlay($image, $logo, Easy\Position::create(30, 20), 50)->show();
 
 /*
-// Chargement d'un image pour le fond
-$logo = Easy\Transformation::resizeByPercent(Easy\Image::createFrom('http://www.php.net/images/logos/php-med-trans.png'), 0.2)->saveAlpha();
+  // Chargement d'un image pour le fond
+  $logo = Easy\Transformation::resizeByPercent(Easy\Image::createFrom('http://www.php.net/images/logos/php-med-trans.png'), 0.2)->saveAlpha();
 
-// Création d'une nouvelle image
-$image = Easy\Image::create(Easy\Dimension::create(200, 200))->saveAlpha();
+  // Création d'une nouvelle image
+  $image = Easy\Image::create(Easy\Dimension::create(200, 200))->saveAlpha();
 
-// Définition du carrelage
-imagesettile($image->getImg(), $logo->getImg());
+  // Définition du carrelage
+  imagesettile($image->getImg(), $logo->getImg());
 
-// Répétition de l'image
-imagefilledrectangle($image->getImg(), 0, 0, $image->getWidth()-1, $image->getHeight()-1, IMG_COLOR_TILED);
+  // Répétition de l'image
+  imagefilledrectangle($image->getImg(), 0, 0, $image->getWidth()-1, $image->getHeight()-1, IMG_COLOR_TILED);
 
-$image->show();
+  $image->show();
  * 
  */
 ?>
