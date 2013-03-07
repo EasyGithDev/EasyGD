462c2ac7c49f4724fcf579f53a65769d313fce6e
EasyGithDev <florent.brusciano@gmail.com>
2013-03-07 01:50:28 +0100

    Adding the getFontlist method

--- Easy/text.php
+++ Easy/text.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * EasyGd - a PHP framework for use GD easier
  *
@@ -28,6 +29,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 namespace Easy;
 
 /**
@@ -37,13 +39,12 @@ namespace Easy;
  * @since   1.0.0
  */
 class Text {
+
     const TEXT_DRAW_HORIZONTAL = 1;
     const TEXT_DRAW_VERTICAL = 2;
-
     const TEXT_FONT_DEFAULT = 1;
     const TEXT_FONT_TRUETYPE = 2;
     const TEXT_FONT_FREETYPE = 3;
-
     const TEXT_MACOS_FONT_PATH = '/Library/Fonts';
     const TEXT_WINDOWS_FONT_PATH = 'C:\\Windows\\fonts';
     const TEXT_UNIX_FONT_PATH = '/usr/share/fonts';
@@ -57,84 +58,94 @@ class Text {
     protected $fontfile;
 
     public function __construct($text, $size = 1, Position $position = NULL, Color $color = NULL) {
-        if (!is_int($size))
-            throw new Exception('Size must be integer');
-        $this->text = $text;
-        $this->size = $size;
-        $this->position = ($position == NULL) ? Position::create() : $position;
-        $this->color = ($color == NULL) ? Color::Black() : $color;
-        $this->drawtype = self::TEXT_DRAW_HORIZONTAL;
-        $this->fonttype = self::TEXT_FONT_DEFAULT;
+	if (!is_int($size))
+	    throw new Exception('Size must be integer');
+	$this->text = $text;
+	$this->size = $size;
+	$this->position = ($position == NULL) ? Position::create() : $position;
+	$this->color = ($color == NULL) ? Color::Black() : $color;
+	$this->drawtype = self::TEXT_DRAW_HORIZONTAL;
+	$this->fonttype = self::TEXT_FONT_DEFAULT;
     }
 
     public static function create($text) {
-        return new self($text);
+	return new self($text);
+    }
+
+    public static function getFontList($fontDir) {
+	$list = array();
+	foreach (new \DirectoryIterator($fontDir) as $file) {
+	    if (preg_match('/\.ttf$/', $file->getFilename())) {
+		$list[] = $file->getFilename();
+	    }
+	}
+	return $list;
     }
 
     public function getText() {
-        return $this->text;
+	return $this->text;
     }
 
     public function setText($text) {
-        $this->text = $text;
-        return $this;
+	$this->text = $text;
+	return $this;
     }
 
     public function getColor() {
-        return $this->color;
+	return $this->color;
     }
 
     public function setColor(Color $color) {
-        $this->color = $color;
-        return $this;
+	$this->color = $color;
+	return $this;
     }
 
     public function getPosition() {
-        return $this->position;
+	return $this->position;
     }
 
     public function setPosition(Position $position) {
-        $this->position = $position;
-        return $this;
+	$this->position = $position;
+	return $this;
     }
 
     public function getSize() {
-        return $this->size;
+	return $this->size;
     }
 
     public function setSize($size) {
-        if (!is_int($size))
-            throw new Exception('Size must be integer');
-        $this->size = $size;
-        return $this;
+	if (!is_int($size))
+	    throw new Exception('Size must be integer');
+	$this->size = $size;
+	return $this;
     }
 
     public function getDrawtype() {
-        return $this->drawtype;
+	return $this->drawtype;
     }
 
     public function setDrawtype($drawtype) {
-        $this->drawtype = $drawtype;
-        return $this;
+	$this->drawtype = $drawtype;
+	return $this;
     }
 
     public function getFonttype() {
-        return $this->fonttype;
+	return $this->fonttype;
     }
 
     public function setFonttype($fonttype) {
-        $this->fonttype = $fonttype;
-        return $this;
+	$this->fonttype = $fonttype;
+	return $this;
     }
 
     public function getFontfile() {
-        return $this->fontfile;
+	return $this->fontfile;
     }
 
     public function setFontfile($fontfile) {
-        imageloadfont($fontfile);
-        $this->fontfile = $fontfile;
-        return $this;
+	imageloadfont($fontfile);
+	$this->fontfile = $fontfile;
+	return $this;
     }
 
 }
